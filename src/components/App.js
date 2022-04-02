bd3b7611a73ecae9cd5a5791bbbce18b9cb22b95
GoranAviani <goranaviani@gmail.com>
2022-04-02 12:30:57 +0200

    exercise 6

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Validator from "./Validator";
 
 const App = () =>{
     return (
-        <div>
-            App
+         <div>
+            <Validator />
+            <link
+              rel="stylesheet"
+              href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+            />
         </div>
     )
 }
